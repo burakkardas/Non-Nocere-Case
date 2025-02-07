3f142ca938434cbc8c98d47520a58a5fce37d037
Burak Kardaş <krdsbrk@gmail.com>
2025-02-08 02:43:07 +0300

    Update material properties and shaders for various models to enhance visual fidelity and interaction

---
+++ Assets/_Scripts/GameCore/Model/ModelMaterialInitializer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ModelMaterialInitializer : MonoBehaviour
+{
+    [SerializeField] private List<Material> shaderMaterials;
+    private Color _modelColor;
+
+    [ContextMenu("Set Model Materials")]
+    public void SetModelMaterials()
+    {
+        foreach (var model in GetAllChildren(transform))
+        {
+            var meshRenderer = model.GetComponent<MeshRenderer>();
+            if (meshRenderer == null) continue;
+
+            // Orijinal rengini kaydet
+            _modelColor = meshRenderer.sharedMaterial.color;
+
+            // Material eşleşmesi yap
+            var originalMaterialName = meshRenderer.sharedMaterial.name.Replace(" (Instance)", "");
+            var newMaterial = shaderMaterials.FirstOrDefault(x => x.name == originalMaterialName);
+
+            if (newMaterial != null)
+            {
+                meshRenderer.sharedMaterial = newMaterial;
+                meshRenderer.sharedMaterial.color = _modelColor;
+            }
+        }
+    }
+
+    private IEnumerable<Transform> GetAllChildren(Transform parent)
+    {
+        return parent.GetComponentsInChildren<Transform>().Where(t => t != parent);
+    }
+}
